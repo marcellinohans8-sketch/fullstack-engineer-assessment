22fd267088c792c3371d3f9ca916030f122b3767
marcellinohans8-sketch <marcellinohans8@gmail.com>
2026-07-21 06:04:46 +0700

    feat: implement search input

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+import { TextInput, StyleSheet } from "react-native";
+
+interface Props {
+  value: string;
+  onChangeText: (text: string) => void;
+}
+
+export default function SearchBar({ value, onChangeText }: Props) {
+  return (
+    <TextInput
+      placeholder="Search task..."
+      value={value}
+      onChangeText={onChangeText}
+      style={styles.input}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: "#fff",
+    margin: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+});
--- frontend/src/screens/TaskScreen.tsx
+++ frontend/src/screens/TaskScreen.tsx
@@ -9,16 +9,21 @@ import {
 
 import { getTasks } from "../api/taskApi";
 import { Task } from "../types/task";
+import SearchBar from "../components/SearchBar";
 
 export default function TaskScreen() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const [keyword, setKeyword] = useState("");
+
   const fetchTasks = async () => {
     try {
       setLoading(true);
 
-      const response = await getTasks();
+      const response = await getTasks({
+        keyword,
+      });
 
       setTasks(response.data.data);
     } catch (error) {
@@ -30,7 +35,7 @@ export default function TaskScreen() {
 
   useEffect(() => {
     fetchTasks();
-  }, []);
+  }, [keyword]);
 
   if (loading) {
     return (
@@ -41,21 +46,25 @@ export default function TaskScreen() {
   }
 
   return (
-    <FlatList
-      data={tasks}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <View style={styles.card}>
-          <Text style={styles.title}>{item.title}</Text>
+    <>
+      <SearchBar value={keyword} onChangeText={setKeyword} />
 
-          <Text>{item.description}</Text>
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Text style={styles.title}>{item.title}</Text>
 
-          <Text>Status : {item.status}</Text>
+            <Text>{item.description}</Text>
 
-          <Text>Assignee : {item.assignee}</Text>
-        </View>
-      )}
-    />
+            <Text>Status : {item.status}</Text>
+
+            <Text>Assignee : {item.assignee}</Text>
+          </View>
+        )}
+      />
+    </>
   );
 }
 
